798c621a4a2a6b37142e8dd741b3316d0752862a
Barış Üçkardeş <baris.uckardess@gmail.com>
2021-01-19 00:32:41 +0300

    Add project files.

---
+++ EntryPoint/ProgramEntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntryPoint
+{
+    class ProgramEntryPoint
+    {
+        static int Main()
+        {
+          
+            Console.ReadKey();
+            return -1;
+        }
+    }
+}
